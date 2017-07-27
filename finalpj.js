b188cd298ca4da7bf3bb8ba12a8c15d8a039edb3
Chris <christurin01@gmail.com>
2017-07-27 14:00:26 -0400

    profile

---
+++ finalpj.js
@@ -0,0 +1,24 @@
+// function extend(){
+//   var name = document.getElementsByClassName('btn btn-info btn-lg');
+// name.setAttribute("class", "down");
+// }
+// When page is loaded do the following function
+// $(document).ready(function() {
+// // when glyphicon is clicked, do the following function
+// 	$(".glyphicon").click(function () {
+// 		$(".menu").slideToggle(2000);
+//
+// 	})
+//
+// })
+
+// a = 0;
+// function show(){
+// 	a++;
+// 	if (a % 2 == 1){
+// 		document.getElementsByClassName('menu').style.display = "none";
+// 	}
+// 	else {
+// 		document.getElementsByClassName('menu').style.display = "block";
+// 	}
+// }
