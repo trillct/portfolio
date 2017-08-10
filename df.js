00a3267ce3b27184f7eb3a798e13c13dabc06171
Chris <christurin01@gmail.com>
2017-08-09 23:42:11 -0400

    new update

--- df.js
+++
@@ -1,9 +0,0 @@
-// When page is loaded do the following function
-$(document).ready(function() {
-// when glyphicon is clicked, do the following function
-	$(".glyphicon").click(function () {
-		$(".menu").slideToggle(2000);
-
-	})
-
-})
